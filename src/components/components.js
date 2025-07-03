a342a425ab91ebaba082d40cbcee90e419143fa1
= <=iamustapha213@gmail.com>
2025-07-03 18:06:47 +0100

    API added, now testing stage II

--- src/components/components.js
+++ src/components/components.js
@@ -317,7 +317,7 @@ export let DishComponent = (props) => {
 	};
 	useEffect(() => {
 		if (update == true) {
-			navigates(`/client/vendor/${props.obj.id}`);
+			navigates(`/client/vendor/dish/${props.obj.id}`);
 		}
 	}, [update]);
 	return (
@@ -338,7 +338,7 @@ export let DishComponent = (props) => {
 					<del>{props.obj.discount}</del>
 				</span>
 				<a
-					href={`/client/dish/${props.obj.id}`}
+					href={`/client/vendor/dish/${props.obj.id}`}
 					element={<Dish />}>
 					<button
 						class='regular-btn'
